2741e4ba45806e87332ff2f39a15ad39b56f4ad8
Brycen Harmon <brycenharmon@gmail.com>
2023-11-27 07:55:09 -0600

    Added images, and a declaration file so images could be imported correctly

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
 import "./Navbar.scss"
 import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { Link } from "react-router-dom";
 
 interface NavbarProps {
@@ -11,7 +12,8 @@ const Navbar: FunctionComponent<NavbarProps> = () => {
     return ( 
         <div className="nav-bar">
             <Link className='logo' to='/'>
-                <img src="{LogoS}" alt="" />
+                <img src={LogoS} alt="logo" />
+                <img src={LogoSubtitle} alt="logosub" />
             </Link>
         </div>
      );
